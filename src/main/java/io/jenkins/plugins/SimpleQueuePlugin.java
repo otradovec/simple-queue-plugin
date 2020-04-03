72a348a6e3db132b4e75bffbf19a5f91691d1f38
otradovec <otradovec@github.com>
2020-04-03 10:09:18 +0200

    Remove default queue widget

---
+++ src/main/java/io/jenkins/plugins/SimpleQueuePlugin.java
@@ -0,0 +1,23 @@
+package io.jenkins.plugins;
+
+import hudson.Plugin;
+import hudson.model.Queue;
+import hudson.widgets.Widget;
+import jenkins.model.Jenkins;
+
+import java.util.logging.Logger;
+
+public class SimpleQueuePlugin extends Plugin {
+    private final static Logger logger = Logger.getLogger(SimpleQueuePlugin.class.getName());
+
+    @Override
+    public void postInitialize() throws Exception {
+        super.postInitialize();
+        if (Jenkins.get().getWidgets().isEmpty()){
+            logger.info("Empty widget list!");
+        }else {
+            Widget queueWidget = Jenkins.get().getWidgets().get(0);
+            Jenkins.get().getWidgets().remove(queueWidget);
+        }
+    }
+}
