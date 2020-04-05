23a81053c51875e833b2c7f5afde9d74130605a8
otradovec <otradovec@github.com>
2020-04-05 17:38:59 +0200

    Ajax queue

--- src/main/java/cz/mendelu/xotradov/SimpleQueueUpdateAction.java
+++ src/main/java/cz/mendelu/xotradov/SimpleQueueUpdateAction.java
@@ -3,14 +3,30 @@ package cz.mendelu.xotradov;
 
 import hudson.Extension;
 import hudson.model.*;
+import jenkins.model.Jenkins;
 
+import java.util.logging.Logger;
+
+@SuppressWarnings("unused")
 @Extension
 public class SimpleQueueUpdateAction implements RootAction {
-    /*public final Queue queue;
+    private final static Logger logger = Logger.getLogger(SimpleQueueUpdateAction.class.getName());
+    public final Queue queue;
 
+    public SimpleQueueUpdateAction(){
+        queue = Jenkins.get().getQueue();
+    }
     public SimpleQueueUpdateAction(Queue queue) {
         this.queue = queue;
-    }*/
+    }
+    public Queue.Item[] getItems(){
+        return queue.getItems();
+    }
+
+    public boolean isFilterQueue(){
+        return false;
+        // TODO return podle queue
+    }
 
     public String getIconFileName() {
         return null;
@@ -23,6 +39,5 @@ public class SimpleQueueUpdateAction implements RootAction {
     public String getUrlName() {
         return "updateQueue";
     }
-
 
 }
--- src/main/java/cz/mendelu/xotradov/SimpleQueueWidget.java
+++ src/main/java/cz/mendelu/xotradov/SimpleQueueWidget.java
@@ -3,6 +3,7 @@ package cz.mendelu.xotradov;
 import hudson.Extension;
 import hudson.widgets.Widget;
 
+@SuppressWarnings("unused")
 @Extension
 public class SimpleQueueWidget extends Widget {
 }
