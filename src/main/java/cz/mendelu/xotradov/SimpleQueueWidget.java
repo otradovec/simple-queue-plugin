63bf175258d2e611ba6313ff227328d2e65a14ac
otradovec <otradovec@github.com>
2020-04-16 16:29:58 +0200

    Shorter updating: index.jelly & widget class

--- src/main/java/cz/mendelu/xotradov/SimpleQueueWidget.java
+++ src/main/java/cz/mendelu/xotradov/SimpleQueueWidget.java
@@ -1,9 +1,19 @@
 package cz.mendelu.xotradov;
 
 import hudson.Extension;
+import hudson.model.Queue;
 import hudson.widgets.Widget;
+import jenkins.model.Jenkins;
 
 @SuppressWarnings("unused")
 @Extension(ordinal=199)
 public class SimpleQueueWidget extends Widget {
+    public Queue.Item[] getItems(){
+        return Jenkins.get().getQueue().getItems();
+    }
+
+    public boolean isFilterQueue(){
+        return false;
+        // TODO return podle queue
+    }
 }
